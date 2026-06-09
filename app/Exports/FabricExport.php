4391156e93261718d4e89da49d78591d6960fef1
DewaPuspito <dewactra02@gmail.com>
2026-06-09 08:50:59 +0700

    add fabric export

--- app/Exports/FabricExport.php
+++ app/Exports/FabricExport.php
@@ -15,4 +15,35 @@ class FabricExport implements FromCollection, WithHeadings
     {
         return Fabric::all();
     }
+    
+    public function headings(): array
+    {
+        return ['Serial Number', 
+        'Tipe', 
+        'Label', 
+        'Merk Mesin', 
+        'Bagian', 
+        'Tahun', 
+        'Check Gear Box (Bulanan)', 
+        'Ganti Oli Gear Box (Bulanan)', 
+        'Check Tegangan Rantai (Bulanan)', 
+        'Check Electrical dan Motor (Bulanan)', 
+        'Kebersihan Mesin (Bulanan)', 
+        'Pemberian Grease pada Roller (Bulanan)', 
+        'Check Karat Mesin (Bulanan)', 
+        'Komponen Lainnya (Bulanan)', 
+        'Dicek Tanggal', 
+        'Tahun Pembelian', 
+        'Perbaikan Gear Box (Harian)', 
+        'Ganti Oli Gear Box (Harian)', 
+        'Perbaikan Tegangan Rantai (Harian)', 
+        'Perbaikan Electrical dan Motor (Harian)', 
+        'Kebersihan Mesin (Harian)', 
+        'Pemberian Grease pada Roller (Harian)', 
+        'Ganti Karet Roller (Harian)', 
+        'Komponen Lainnya (Harian)',  
+        'Jenis Sparepart yang Diganti', 
+        'Tanggal Sparepart Diganti', 
+        'Harga Sparepart'];
+    }
 }
